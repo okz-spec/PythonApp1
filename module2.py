e024a2a7dfc788c6fd08546ae0b6f78f7570178c
opilane <opilane@HP-CZC2349HTS>
2025-09-25 11:04:25 +0300

    ?

--- module2.py
+++ module2.py
@@ -11,6 +11,6 @@ print("Kas käib koolis on tüüp:", type(kas_käib_koolis))
 kas_käib_koolis = False
 
 if isinstance(kas_käib_koolis, bool):
-    print("Muuttuja 'kas_käib_koolis' on boolean tüüpi.")
+    print("Muuttuja 'kas_käib_koolis' on boolean tüüpi.")   
 else:
     print("Muuttuja 'kas_käib_koolis' ei ole boolean tüüpi.")
